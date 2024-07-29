368549f3b5180246141c5a8ab719a4aec7d5b829
Chanthima Tosungneon <chanthima.t@astartechs.com>
2024-07-29 16:58:06 +0700

    dev_a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
+import "../styles/styles.css";
 import { Prompt } from 'next/font/google'
 import ThemeRegistry from '../components/Theme/ThemeRegistry';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import Navbar from "../layout/Navbar";
+import Sidebar from "../layout/Sidebar";
 
 const prompt = Prompt({
   weight: ['300', '400', '500', '700'],
@@ -24,7 +27,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={prompt.className}>
         <ThemeRegistry>
-          {children}
+          <Navbar />
+          <div className="globals-layout">
+            <div className="flex h-[calc(100vh-56px)] ">
+              <div className="hidden md:block border-e pr-6 ">
+                <Sidebar />
+              </div>
+              {/* ถ้ามี Sidebar ให้เพิ่ม md:px */}
+              <div className="w-[100vw] md:px-6">
+                {children}
+              </div>
+            </div>
+          </div>
         </ThemeRegistry>
       </body>
     </html>
---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Avatar } from 'primereact/avatar';
+import { MegaMenu } from 'primereact/megamenu';
+import { items } from '../router/menuData';
+
+export default function Navbar() {
+    return (
+        <div className='globals-padding border-b shadow-md'>
+            <nav className=" flex justify-between items-center">
+                {/* logo & name web */}
+                <a href="/" className="flex items-center gap-2">
+                    <Avatar image="favicon.ico" size="large" />
+                    <div className="">example</div>
+                </a>
+                <div className=" flex items-center max-lg:flex-row-reverse gap-2">
+                    {/* menu */}
+
+                    {/* แก้ไข MegaMenu  bg text border  class .p-megamenu   */}
+                    {/* <MegaMenu model={items} breakpoint="1280px" /> */}
+                    {/* user */}
+                    <div className="flex items-center gap-2">
+                        <div className=" max-sm:hidden">Admin@gmail.com</div>
+                        <Avatar image="favicon.ico" label="Text with image" />
+
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+    )
+}
